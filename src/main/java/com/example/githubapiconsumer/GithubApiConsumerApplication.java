9a0bbfdcd783e26dc632a95135e70e7aada42dc4
Daniel Dąbrowski <danieldabrowskikontakt@gmail.com>
2024-03-13 09:33:57 +0100

    Init open api,swagger ui and add yaml api file

--- src/main/java/com/example/githubapiconsumer/GithubApiConsumerApplication.java
+++ src/main/java/com/example/githubapiconsumer/GithubApiConsumerApplication.java
@@ -3,6 +3,8 @@ package com.example.githubapiconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class GithubApiConsumerApplication {
 
